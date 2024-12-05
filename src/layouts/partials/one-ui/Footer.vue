1d7bc2d9d64a1e8086bbad1bd0a9eeaa4fd57a6a
Do Huy <huy.d@vlu.edu.vn>
2024-12-05 11:47:59 +0700

    feat: UI master page Tạo giao diện master page view quản trị index.html logo-khong-chu.png _variables-bootstrap.scss BaseLayout.vue master-page/Breadcrumb.vue one-ui/Footer.vue one-ui/Header.vue one-ui/SideOverlay.vue one-ui/Sidebar.vue master-page/AdminMasterPage.vue master-page/MasterPage.vue one-ui/AdminLayout.vue index.js template.js AdminMasterView.vue Task 791

---
+++ src/layouts/partials/one-ui/Footer.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { useTemplateStore } from "@/stores/template";
+
+// Main store
+const store = useTemplateStore();
+</script>
+
+<template>
+  <!-- Footer -->
+  <footer id="page-footer">
+    <slot>
+      <div class="bg-body-light">
+        <div class="content py-3">
+          <div class="row fs-sm">
+            <div class="col-sm order-sm-2 py-1 text-end text-sm-end">
+              <slot name="content-right">
+                Phát triển bởi
+                <span class="text-primary">Phòng Công Nghệ Thông Tin</span>
+              </slot>
+            </div>
+          </div>
+        </div>
+      </div>
+    </slot>
+  </footer>
+  <!-- END Footer -->
+</template>
